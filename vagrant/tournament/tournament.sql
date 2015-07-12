990e6086e46155af7c878ffbd23e1170cb11de28
Greg Bullmer <gsbullmer@gmail.com>
2015-07-12 13:56:03 -0500

    Fix: Change foreign key declarations

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,23 +1,22 @@
 -- Table definitions for the tournament project.
---
+-- 
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
---
+-- 
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
 
 CREATE TABLE players (
-    id SERIAL PRIMARY KEY,
+    player_id SERIAL PRIMARY KEY,
     name TEXT
-)
+);
 
 CREATE TABLE matches (
-    id SERIAL PRIMARY KEY,
-    winner INT,
-    loser INT,
-    FOREIGN KEY (winner, loser) REFERENCES players (id, id)
-)
+    match_id SERIAL PRIMARY KEY,
+    winner INT REFERENCES players (player_id),
+    loser INT REFERENCES players (player_id)
+);
 
 -- Extra credit
 -- CREATE TABLE tournaments (
